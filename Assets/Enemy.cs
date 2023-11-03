ff8f233fd291923394548f82448b8841af11ea09
pinjakemppainen <pinja.kemppainen02@gmail.com>
2023-11-03 14:57:51 +0200

    Deleting files

---
+++ Assets/Enemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 3.0f;
+    public float detectionRange = 10.0f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        // Check the distance between the enemy and the player
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= detectionRange)
+        {
+            // Move towards the player
+            Vector3 direction = player.position - transform.position;
+            direction.Normalize();
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+}
+
