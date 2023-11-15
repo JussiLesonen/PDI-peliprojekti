9f98c95eac0b0654beaa991923aeb6976871eedf
TeeAr00 <t1rate00@students.oamk.fi>
2023-11-15 13:08:32 +0200

    Gravity
    Gravity flip camera smoothing.
    Jumping fix while gravity is upside.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,8 +15,6 @@ public class Player : MonoBehaviour
     public CinemachineFreeLook playerCam;
 
 
-
-
     public float speed;
     public float gravity = -9.81f;
     public float jumpHeight = 3;
@@ -34,22 +32,24 @@ public class Player : MonoBehaviour
 
 
 
+
+
     void Update()
     {
         // Gravity flip
         //Change hardcoded gravity change while on roof
         //make sure you check isGrounded from the roof also
-        if (Input.GetKeyDown(KeyCode.H))
+        if (Input.GetKeyDown(KeyCode.H) && isGrounded)
         {
             isGravityFlipped = !isGravityFlipped;
 
             // Change gravity direction
             gravity = isGravityFlipped ? 9.81f : -9.81f;
 
-            velocity.y = 0f;
+            //velocity.y = 0f;
 
-            Vector3 currentRotation = transform.eulerAngles;
-            transform.eulerAngles = new Vector3(currentRotation.x, currentRotation.y, currentRotation.z + 180f);
+            
+            //transform.eulerAngles = new Vector3(currentRotation.x, currentRotation.y, currentRotation.z + 180f);
 
             //playerCam.transform.rotation *= Quaternion.Euler(0, 0, 180);
             UpdatePlayerCamFollowTarget();
@@ -61,24 +61,41 @@ public class Player : MonoBehaviour
         }
 
         //jump
+        Vector3 currentRotation = transform.eulerAngles;
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0)
+        
+        if (!isGravityFlipped)
         {
-            velocity.y = -2f;
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+        }
+        else
+        {
+            if (isGrounded && velocity.y > 0)
+            {
+                velocity.y = 2f;
+            }
         }
 
         if (Input.GetButtonDown("Jump"))
         {
             if (!isGravityFlipped && isGrounded)
             {
+
+                //velocity.y = -2f;
                 velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
                 jumpSound.Play();
+                Debug.Log(velocity.y);
             }
             if (isGravityFlipped && isGrounded)
             {
+                //velocity.y = 2f;
                 velocity.y = Mathf.Sqrt(jumpHeight * -2 * -gravity)*-1;
                 jumpSound.Play();
+                Debug.Log(velocity.y);
             }
         }
 
@@ -89,6 +106,7 @@ public class Player : MonoBehaviour
         //walk
         if (!isGravityFlipped)
         {
+            transform.eulerAngles = new Vector3(currentRotation.x, currentRotation.y, Mathf.Lerp(currentRotation.z, 0, Time.deltaTime * 10f));
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
             Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -109,6 +127,7 @@ public class Player : MonoBehaviour
         }
         else if (isGravityFlipped)
         {
+            transform.eulerAngles = new Vector3(currentRotation.x, currentRotation.y, Mathf.Lerp(currentRotation.z, 180f, Time.deltaTime * 10f));
             float horizontal = -Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
             Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
