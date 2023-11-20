bf59bd9e52290e819825e6c541c5887ef76863bc
pinjakemppainen <pinja.kemppainen02@gmail.com>
2023-11-20 13:53:22 +0200

    Enemy shooting (not done)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,7 +7,14 @@ public class Enemy : MonoBehaviour
     public Transform player;
     public float moveSpeed = 3.0f;
     public float detectionRange = 5.0f;
-
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float shootCooldown = 2.0f;
+    public float bulletSpeed = 10.0f;
+    public LayerMask playerLayer;
+    [SerializeField]private float timer = 5;
+    private float bulletTime;
+    private float lastShootTime;
     private bool isPatrolling = false;
     private Vector3 originalPosition;
     public float patrolRange = 3.0f;
@@ -16,10 +23,12 @@ public class Enemy : MonoBehaviour
     {
         player = GameObject.Find("Player").transform;
         originalPosition = transform.position;
+        lastShootTime = Time.time;
     }
 
     void Update()
     {
+
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
         if (distanceToPlayer <= detectionRange)
@@ -27,10 +36,17 @@ public class Enemy : MonoBehaviour
             // Player is detected, switch to follow player state
             isPatrolling = false;
             FollowPlayer();
+
+            // Shoot at the player
+            if (Time.time - lastShootTime > shootCooldown)
+            {
+                Shoot();
+                lastShootTime = Time.time;
+            }
         }
         else
         {
-            // Player is not detected, switch to patrol state
+            // Player is not detected, switch back to patrol state
             Patrol();
         }
     }
@@ -46,12 +62,10 @@ public class Enemy : MonoBehaviour
     {
         if (!isPatrolling)
         {
-            // Move towards the original position
             Vector3 direction = originalPosition - transform.position;
             direction.Normalize();
             transform.Translate(direction * moveSpeed * Time.deltaTime);
 
-            // Check if reached the original position
             float distanceToOriginal = Vector3.Distance(transform.position, originalPosition);
             if (distanceToOriginal < 0.1f)
             {
@@ -63,12 +77,26 @@ public class Enemy : MonoBehaviour
             // Move within the patrol range
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
 
-            // Check if reached the patrol range limit
             float distanceToPatrolLimit = Mathf.Abs(transform.position.x - originalPosition.x);
             if (distanceToPatrolLimit >= patrolRange)
             {
                 isPatrolling = false;
             }
         }
+    }
+
+    void Shoot()
+    {
+       
+       bulletTime -= Time.deltaTime;
+
+       if (bulletTime > 0) return;
+
+       bulletTime = timer;
+
+       GameObject bulletObj = Instantiate(bullet, spawnPoint.transform.position, spawnPoint.transform.rotation) as GameObject;
+       Rigidbody bulletRig = bulletObj.GetComponent<Rigidbody>();
+       bulletRig.AddForce(bulletRig.transform.forward * moveSpeed);
+       Destroy(bulletObj, 0.1f);
     }
 }
