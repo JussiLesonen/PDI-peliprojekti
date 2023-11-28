4e9b4c9dd60a74dadcd64596b3cb75f016cb525a
TeeAr00 <t1rate00@students.oamk.fi>
2023-11-28 15:21:47 +0200

    Teleport
    Teleport object added to the game.
    bug fixes for the gravity change.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -28,6 +28,15 @@ public class Player : MonoBehaviour
     float turnSmoothVelocity;
     float jumpPECooldown;
 
+    public void Teleport(Vector3 position, Quaternion rotation)
+    {
+        transform.position = position;
+        transform.rotation = rotation;
+        Physics.SyncTransforms();
+        Debug.Log("teepee");
+        
+    }
+
     public float turnSmoothTime = 0.1f;
     void Update()
     {
@@ -35,20 +44,12 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.H) && isGrounded)
         {
             isGravityFlipped = !isGravityFlipped;
-
-            // Change gravity direction
             gravity = isGravityFlipped ? 9.81f : -9.81f;
-
-            //transform.eulerAngles = new Vector3(currentRotation.x, currentRotation.y, currentRotation.z + 180f);
-            //playerCam.transform.rotation *= Quaternion.Euler(0, 0, 180);
-            UpdatePlayerCamFollowTarget();
         }
-
         //jump
         Vector3 currentRotation = transform.eulerAngles;
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        
         if (!isGravityFlipped)
         {
             if (isGrounded && velocity.y < 0)
@@ -68,14 +69,12 @@ public class Player : MonoBehaviour
         {
             if (!isGravityFlipped && isGrounded)
             {
-                //velocity.y = -2f;
                 velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
                 jumpSound.Play();
                 Debug.Log(velocity.y);
             }
             if (isGravityFlipped && isGrounded)
             {
-                //velocity.y = 2f;
                 velocity.y = Mathf.Sqrt(jumpHeight * -2 * -gravity)*-1;
                 jumpSound.Play();
                 Debug.Log(velocity.y);
@@ -97,14 +96,7 @@ public class Player : MonoBehaviour
             if (direction.magnitude >= 0.1f)
             {
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-
-                // Calculate the movement direction based on the target angle
                 Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-                // Keep the original Y rotation and only update the position
-                Quaternion newRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
-                transform.rotation = newRotation;
-
                 controller.Move(moveDir.normalized * speed * Time.deltaTime);
             }
         }
@@ -118,27 +110,17 @@ public class Player : MonoBehaviour
             if (direction.magnitude >= 0.1f)
             {
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-
-                // Calculate the movement direction based on the target angle
                 Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-                // Keep the original Y rotation and only update the position
-                Quaternion newRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
-                transform.rotation = newRotation;
-
                 controller.Move(moveDir.normalized * speed * Time.deltaTime);
             }
         }
-        
-        void UpdatePlayerCamFollowTarget()
-        {
-            playerCam.transform.rotation *= Quaternion.Euler(0, 0, 180);
-            //playerCam.m_Orbits[0].m_Height = -playerCam.m_Orbits[0].m_Height;
-            //playerCam.m_Orbits[1].m_Height = -playerCam.m_Orbits[1].m_Height;
-            //playerCam.m_Orbits[2].m_Height = -playerCam.m_Orbits[2].m_Height;
-        }
-
-
+        //void UpdatePlayerCamFollowTarget()
+        //{
+        //    playerCam.transform.rotation *= Quaternion.Euler(0, 0, 180);
+        //    playerCam.m_Orbits[0].m_Height = -playerCam.m_Orbits[0].m_Height;
+        //    playerCam.m_Orbits[1].m_Height = -playerCam.m_Orbits[1].m_Height;
+        //    playerCam.m_Orbits[2].m_Height = -playerCam.m_Orbits[2].m_Height;
+        //}
         if (isDead)
         {
             playerCam.Follow = null;
