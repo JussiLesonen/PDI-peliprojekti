550087a23c0d3e434cbdddec9ea2bb2a4092d64a
JuhoSoivio <101523348+JuhoSoivio@users.noreply.github.com>
2023-11-30 17:00:56 +0200

    update
    update

--- Assets/Scripts/Objectives.cs
+++ Assets/Scripts/Objectives.cs
@@ -5,11 +5,15 @@ using TMPro;
 
 public class Objectives : MonoBehaviour
 {
-    public TextMeshProUGUI objectiveText;
+    TextMeshProUGUI objectiveText;
     public static bool canSpawn = true;
     public static float timeValue = 0;
+    public static int level = 1;
     bool timeStarted = false;
-
+    private void Start()
+    {
+        objectiveText = GameObject.Find("ObjectivesText").GetComponent<TextMeshProUGUI>();
+    }
     void Update()
     {
         if (timeValue <= 0)
@@ -32,17 +36,20 @@ public class Objectives : MonoBehaviour
             {
                 timeStarted = true;
             }
-            if (timeValue < 0.1f && GameObject.Find("Key(Clone)")==null)
+            if (timeValue < 0.1f && GameObject.Find("Key(Clone)") == null)
             {
-                Instantiate(Resources.Load("Key"), transform.position,
+                Instantiate(Resources.Load("Key"), new Vector3(-0.06f, 0.96f, 30.64f),
                 Quaternion.Euler(Vector3.zero));
+                Debug.Log(Resources.Load("Key(Clone)"));
+                Debug.Log(timeValue);
+                Debug.Log(transform.position);
             }
             objectiveText.text = "Go to the next stage within " + Mathf.Round(timeValue).ToString() + "s";
         }
-        if (timeStarted && timeValue < 0.1f)
+        if (timeStarted && timeValue < 0.1f && level == 1)
         {
             objectiveText.text = "Objective: Collect the key";
         }
-        
+
     }
 }
